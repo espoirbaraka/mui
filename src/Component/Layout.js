6cad4f51d5e4250e768aabc3cfeaafaab08c8da0
Baraka Bigega Espoir <49196039+espoirbaraka@users.noreply.github.com>
2022-04-15 10:16:23 +0200

    Doing navbar

--- src/Component/Layout.js
+++ src/Component/Layout.js
@@ -1,37 +1,45 @@
-// import { makeStyles } from '@mui/styles';
+import { makeStyles } from '@mui/styles';
 import React from 'react'
 import { Drawer, Typography } from '@mui/material'
 
 const drawerWidth = 240
 
-// const useStyles = makeStyles({
-//     page: {
-//         background: '#f9f9f9',
-//         width: '100%'
-//     },
-//     drawer:{
-//         width: drawerWidth
-//     }
-// })
+const useStyles = makeStyles({
+    page: {
+        background: '#f9f9f9',
+        width: '100%'
+    },
+    drawer: {
+        width: drawerWidth
+    },
+    drawerPaper: {
+        width: drawerWidth
+    },
+    root: {
+        display: 'flex'
+    }
+})
 
-function Layout({children}) {
-    // const classes = useStyles()
+function Layout({ children }) {
+    const classes = useStyles()
     return (
-        <div>
-            {/*<Drawer
-            className={classes.drawer}
-            variant="permanent"
-            anchor='left'
+        <div className={classes.root}>
+            <Drawer
+                className={classes.drawer}
+                variant="permanent"
+                anchor='left'
+                classes={{paper: classes.drawerPaper}}
             >
                 <div>
                     <Typography variant='h5'>
-                    ISIG
+                        ISIG goma
                     </Typography>
                 </div>
             </Drawer>
             <div className={classes.page}>
-    </div>*/}
-        	{children}
+                {children}
+            </div>
+
         </div>
     )
 }
