d129cc99d755cbbdc6c293a1ebb36dc130604892
Baraka Bigega Espoir <49196039+espoirbaraka@users.noreply.github.com>
2022-04-18 08:22:22 +0200

    Doing navbar

--- src/Component/Layout.js
+++ src/Component/Layout.js
@@ -4,22 +4,32 @@ import { Drawer, ListItemIcon, Typography } from '@mui/material'
 import { List, ListItem, ListeItemIcon, ListItemText } from '@mui/material'
 import { AddCircleOutlineOutlined, SubjectOutlined } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
+import {AppBar, Toolbar} from '@mui/material'
 
 const drawerWidth = 240
 
-const useStyles = makeStyles({
-    page: {
-        background: '#f9f9f9',
-        width: '100%'
-    },
-    drawer: {
-        width: drawerWidth
-    },
-    drawerPaper: {
-        width: drawerWidth
-    },
-    root: {
-        display: 'flex'
+const useStyles = makeStyles((theme) => {
+    return {
+        page: {
+            background: '#f9f9f9',
+            width: '100%',
+            //padding: theme.spacing(3)
+        },
+        drawer: {
+            width: drawerWidth
+        },
+        drawerPaper: {
+            width: drawerWidth
+        },
+        root: {
+            display: 'flex'
+        },
+        active: {
+            background: '#f4f4f4'
+        },
+        title:{
+            //padding: theme.spacing(2)
+        }
     }
 })
 
@@ -46,17 +56,17 @@ function Layout({ children }) {
             {/*side drawer*/}
             <Drawer className={classes.drawer} variant="permanent" anchor='left' classes={{ paper: classes.drawerPaper }}>
                 <div>
-                    <Typography variant='h5'>
+                <Typography variant='h5' className={classes.title}>
                         ISIG goma
                     </Typography>
                 </div>
                 {/*List drawer*/}
                 <List>
                     {menuItems.map(item => (
-                        <ListItem 
-                        button 
-                        key={item.text}
-                        onClick={()=>history.navigate(item.path)}>
+                        <ListItem
+                            button
+                            key={item.text}
+                            onClick={() => history.push(item.path)}>
                             <ListItemIcon>{item.icon}</ListItemIcon>
                             <ListItemText primary={item.text} />
                         </ListItem>
